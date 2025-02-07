0ca17df7e329636fda5a45c0cc3a3d3e8acb34cb
MuhammadDanishgtr <162627889+MuhammadDanishgtr@users.noreply.github.com>
2025-02-07 20:38:51 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminDashboard from './admin/dashboard/page'
+
+function Home() {
+  return (
+    <div>
+      <AdminDashboard/>
+    </div>
+  )
+}
+
+export default Home
